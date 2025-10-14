b8e2251c9829b7d844056c3df69656a4fd28d238
Sybillemaeva <ndjanaebogo.sybillemaeva@ictuniversity.edu.cm>
2025-10-14 13:38:36 +0100

    fix(header): remove verified badge overlay (prevents tick in hero section)

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -216,14 +216,7 @@ const Header = () => {
                 </div>
               )}
 
-              {/* verified badge when logo was fetched from backend and loaded */}
-              {logoFetchedFromBackend && logoLoaded && (
-                <div className="absolute -bottom-1 -right-1 bg-white rounded-full p-0.5 shadow">
-                  <svg className="h-4 w-4 text-green-600" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                  </svg>
-                </div>
-              )}
+              {/* verification badge removed to avoid overlapping other sections */}
             </div>
           </div>
           
